a1cdfe8b9e1708562b4dd963addb1823750c724b
dengji1 <109778576+dengji1@users.noreply.github.com>
2025-09-29 00:24:25 +0800

    Update refiner_args.py

--- refiner/refiner_args.py
+++ refiner/refiner_args.py
@@ -1,7 +1,7 @@
 class Args:
     def __init__(self, args):
         self.gamma = args.gamma
-        if not args.mix:
+        if not args.pruning:
             self.threads_per_block = 16
             self.blocks_per_grid = 16
             self.threads_per_block_2d = (16, 16)
@@ -11,6 +11,6 @@ class Args:
             self.blocks_per_grid = 2048
             self.threads_per_block_2d = (32, 32)
             self.blocks_per_grid_2d = (128, 128)
-            self.percentile = 99
+            self.percentile = 60
             self.min_bin_num = 512
             self.max_module_per_bin = 500
