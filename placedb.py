394677a9de7bb5c990962abb1cc3a88db4abcbb1
dengji1 <109778576+dengji1@users.noreply.github.com>
2025-09-29 00:09:30 +0800

    Update placedb.py

--- placedb.py
+++ placedb.py
@@ -1,19 +1,82 @@
 import numpy as np
 import os
+from itertools import combinations
 
 class PlaceDB:
-    def __init__(self, benchmark, base_path, pl_path):
+    def __init__(self, benchmark, base_path, pl_path, args):
         self.benchmark = benchmark
-        self.node_info, self.net_info, self.pin_info, self.port_info = {}, {}, {}, {}
+        self.node_info, self.net_info, self.pin_info = {}, {}, {}
+        self.node_info_all = {}
         self.expert_pos = {}
         self.width, self.height = 0, 0
+        self.offset_x, self.offset_y = 0, 0
+        self.openroad = False
+        if 'openroad' in base_path:
+            self.openroad = True
         
-        self.get_size()
+        if not base_path.endswith(args.macro_suffix):
+            base_path_full = base_path
+            base_path += args.macro_suffix
+        else:
+            base_path_full = base_path[:-len(args.macro_suffix)]
+        base_path_full = os.path.join(base_path_full, benchmark)
+        
+        self.get_all_nodes(base_path_full)
+        self.get_orign(base_path_full)
+            
         base_path = os.path.join(base_path, benchmark)
         self.get_node_info(base_path)
         self.get_net_pin_info(base_path)
+        if self.openroad:
+            self.get_size_from_pl(base_path_full)
+        else:
+            self.get_size()
         self.get_expert_pos(pl_path)
+    
+    def get_orign(self, route):
+        path = os.path.join(route, self.benchmark+'.pl')
+        with open(path, 'r') as f:
+            for line in f:
+                parts = line.split()
+                if len(parts) <= 4 or parts[0] == '#':
+                    continue
+                node = parts[0]
+                orign = parts[4]
+                self.node_info_all[node]['orign'] = orign
+    
+    def get_all_nodes(self, route):
+        path = os.path.join(route, self.benchmark+'.nodes')
+        with open(path, 'r') as f:
+            for line in f:
+                node_line = line.split()
+                if not node_line:
+                    continue
+                if line.startswith('\t') or line.startswith(' '):
+                    node_line = line.split()
+                    self.node_info_all[node_line[0]] = {}
+                    self.node_info_all[node_line[0]]['x'] = float(node_line[1])
+                    self.node_info_all[node_line[0]]['y'] = float(node_line[2])
+                    self.node_info_all[node_line[0]]['port'] = False
+                    if len(node_line) > 3 and node_line[3] == 'terminal_NI':
+                        self.node_info_all[node_line[0]]['port'] = True
 
+    def get_size_from_pl(self, route):
+        pl_path = os.path.join(route, self.benchmark+'.ref.pl')
+        xl, yl, xh, yh = float('inf'), float('inf'), float('-inf'), float('-inf')
+        with open(pl_path, 'r') as f:
+            for line in f:
+                parts = line.split()
+                if len(parts) > 4:
+                    node = parts[0]
+                    x, y = float(parts[1]), float(parts[2])
+                    xl = min(x, xl)
+                    yl = min(y, yl)
+                    xh = max(x + self.node_info_all[node]['x'], xh)
+                    yh = max(y + self.node_info_all[node]['y'], yh)
+        self.offset_x, self.offset_y = xl, yl
+        self.width = xh - xl
+        self.height = yh - yl
+    
     def get_node_info(self, route):
         node_id = 0
         path = os.path.join(route, self.benchmark+'.nodes')
@@ -22,20 +85,17 @@ class PlaceDB:
                 node_line = line.split()
                 if not node_line:
                     continue
-                if node_line[0].startswith('p'):
-                        self.port_info[node_line[0]] = {}
-                        self.port_info[node_line[0]]['size_x'] = int(node_line[1])
-                        self.port_info[node_line[0]]['size_y'] = int(node_line[2])
-                        self.port_info[node_line[0]]['nets'] = []
-                else:
-                    if line.startswith('\t') or line.startswith(' '):
-                        node_line = line.split()
-                        self.node_info[node_line[0]] = {}
-                        self.node_info[node_line[0]]['x'] = int(node_line[1])
-                        self.node_info[node_line[0]]['y'] = int(node_line[2])
-                        self.node_info[node_line[0]]['id'] = node_id
-                        self.node_info[node_line[0]]['pins'] = []
-                        node_id += 1
+                if line.startswith('\t') or line.startswith(' '):
+                    node_line = line.split()
+                    self.node_info[node_line[0]] = {}
+                    self.node_info[node_line[0]]['x'] = float(node_line[1])
+                    self.node_info[node_line[0]]['y'] = float(node_line[2])
+                    self.node_info[node_line[0]]['id'] = node_id
+                    self.node_info[node_line[0]]['fix'] = False
+                    self.node_info[node_line[0]]['pins'] = []
+                    if node_line[-1]=='terminal_NI':
+                        self.node_info[node_line[0]]['fix'] = True
+                    node_id += 1
     
     def get_net_pin_info(self, route):
         net_id, pin_id = 0, 0
@@ -47,15 +107,11 @@ class PlaceDB:
                     self.net_info[net_id] = {}
                     self.net_info[net_id]['net_name'] = net_line[3]
                     self.net_info[net_id]['pins'] = []
-                    self.net_info[net_id]['ports'] = []
                     nodes = set()
                     for _ in range(int(net_line[2])):
                         line = f.readline()
                         pin_line = line.split()
-                        if pin_line[0].startswith('p'):
-                            self.port_info[pin_line[0]]['nets'].append(net_id)
-                            self.net_info[net_id]['ports'].append(pin_line[0])
-                        if pin_line[0] not in nodes:
+                        if pin_line[0] not in nodes and pin_line[0] in self.node_info:
                             nodes.add(pin_line[0])
                             self.pin_info[pin_id] = {}
                             self.pin_info[pin_id]['node'] = pin_line[0]
@@ -64,8 +120,17 @@ class PlaceDB:
                             self.pin_info[pin_id]['y'] = float(pin_line[-1])
                             self.net_info[net_id]['pins'].append(pin_id)
                             self.node_info[pin_line[0]]['pins'].append(pin_id)
-                        pin_id += 1
-                    net_id += 1
+                            pin_id += 1
+                        
+                    if len(self.net_info[net_id]['pins']) < 2:
+                        for pin_id1 in self.net_info[net_id]['pins']:
+                            node = self.pin_info[pin_id1]['node']
+                            self.node_info[node]['pins'] = self.node_info[node]['pins'][:-1]
+                            del self.pin_info[pin_id1]
+                            pin_id -= 1
+                        del self.net_info[net_id]
+                    else:
+                        net_id += 1
     
     def get_size(self):
         if (self.benchmark) == 'adaptec1':
@@ -117,19 +182,16 @@ class PlaceDB:
             self.width = 9227160
             self.height = 6101280
     
-    def get_expert_pos(self, route):
-        path = os.path.join(route, self.benchmark+'.pl')
+    def get_expert_pos(self, path):
         with open(path,'r') as f:
             for line in f:
                 pos_line = line.split()
                 if len(pos_line) >= 4 and not line.startswith('#'):
-                    if pos_line[0].startswith('p'):
-                        self.port_info[pos_line[0]]['x'] = float(pos_line[1])
-                        self.port_info[pos_line[0]]['y'] = float(pos_line[2])
-                    else:
+                    if pos_line[0] in self.node_info:
                         self.expert_pos[pos_line[0]] = {}
-                        self.expert_pos[pos_line[0]]['x'] = float(pos_line[1])
-                        self.expert_pos[pos_line[0]]['y'] = float(pos_line[2])
+                        self.expert_pos[pos_line[0]]['x'] = float(pos_line[1]) - self.offset_x
+                        self.expert_pos[pos_line[0]]['y'] = float(pos_line[2]) - self.offset_y
+                        self.expert_pos[pos_line[0]]['orign'] = pos_line[4] 
 
 class RefineDB():
     def __init__(self, placedb: PlaceDB):
@@ -137,9 +199,9 @@ class RefineDB():
         self.id2name_node, self.name2id_node, self.raw_node_pos = [], {}, []
         self.id2name_net, self.name2id_net = [], {}
         self.pin_offset, self.pin2node, self.pin2net = [], [], []
-        self.port_pos, self.port2net = [], []
         self.node_size = np.zeros((len(placedb.node_info), 2))
         self.node_info = {}
+        self.fix_mask = np.zeros(len(placedb.node_info), dtype=np.bool_)
         
         self.get_node_info(placedb)
         self.get_net_info(placedb)
@@ -149,17 +211,27 @@ class RefineDB():
         self.node_cnt = len(self.id2name_node)
         self.pin_cnt = len(self.pin2node)
         self.net_cnt = len(self.id2name_net)
-        self.port_cnt = len(self.port2net)
 
     def get_node_info(self, placedb: PlaceDB):
         cnt = 0
         for node in placedb.node_info:
-            self.id2name_node.append(node)
-            self.name2id_node[node] = cnt
-            x, y = placedb.node_info[node]['x'], placedb.node_info[node]['y']
-            self.node_info[node] = {'x': x, 'y': y}
-            self.node_size[cnt, 0], self.node_size[cnt, 1] = x, y
-            cnt += 1
+            if not placedb.node_info[node]['fix']:
+                self.id2name_node.append(node)
+                self.name2id_node[node] = cnt
+                x, y = placedb.node_info[node]['x'], placedb.node_info[node]['y']
+                self.node_info[node] = {'x': x, 'y': y}
+                self.node_size[cnt, 0], self.node_size[cnt, 1] = x, y
+                self.fix_mask[cnt] = False
+                cnt += 1
+        for node in placedb.node_info:
+            if placedb.node_info[node]['fix']:
+                self.id2name_node.append(node)
+                self.name2id_node[node] = cnt
+                x, y = placedb.node_info[node]['x'], placedb.node_info[node]['y']
+                self.node_info[node] = {'x': x, 'y': y}
+                self.node_size[cnt, 0], self.node_size[cnt, 1] = x, y
+                self.fix_mask[cnt] = True
+                cnt += 1
     
     def get_node_pos(self, placedb:PlaceDB):
         for node in self.id2name_node:
@@ -179,13 +251,8 @@ class RefineDB():
                 self.pin_offset.append([x_offset, y_offset])
                 self.pin2node.append(self.name2id_node[node])
                 self.pin2net.append(self.name2id_net[net])
-            for port in placedb.net_info[net]['ports']:
-                self.port_pos.append([placedb.port_info[port]['x'], placedb.port_info[port]['y']])
-                self.port2net.append(self.name2id_net[net])
             net_cnt += 1
             
         self.pin_offset = np.array(self.pin_offset)
         self.pin2node = np.array(self.pin2node, dtype=np.int32)
         self.pin2net = np.array(self.pin2net, dtype=np.int32)
-        self.port_pos = np.array(self.port_pos)
-        self.port2net = np.array(self.port2net, dtype=np.int32)
